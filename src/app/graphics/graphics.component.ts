61e5d2a1a03732a8558adcdac02c706965d8ad7d
Augusto Pinochet <augusto.pinochet@alumnos.uv.cl>
2022-07-05 21:48:54 -0400

    graficos_v2

--- src/app/graphics/graphics.component.ts
+++ src/app/graphics/graphics.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import {MatTableModule} from '@angular/material/table';
 @Component({
   selector: 'app-gfg',
   templateUrl: './graphics.component.html',
-  styleUrls: ['./graphics.component.css']
+  styleUrls: ['./graphics.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
+
 export class GfgComponent{
     a ="GeeksforGeeks";
 
 }
+
 
 
